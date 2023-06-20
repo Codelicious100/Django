b52725d6aab9f830e1988dcf0481da949c5308c8
Alice_Suh <dongeuns33@gmail.com>
2023-06-19 20:33:33 -0700

    end of the work. finish to make get into individual posts and come back to main blog pg.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 # 다른 파일에 있는 코드를 가져오는 방법
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -7,3 +7,8 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
